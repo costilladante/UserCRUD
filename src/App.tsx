b3253a9a682e270d398cfceebf600b18f4b99646
costilladante <costilladante@gmail.com>
2020-11-30 19:43:31 -0300

    Add antd, husky pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
-function App() {
+function App(): JSX.Element {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
@@ -19,6 +17,7 @@ function App() {
 				>
 					Learn React
 				</a>
+				<Button type="primary">Accept</Button>
 			</header>
 		</div>
 	);
