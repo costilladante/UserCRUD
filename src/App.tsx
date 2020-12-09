da11d4fb7a49707201bfa09cd8e43b323ac791cc
costilladante <costilldante@gmail.com>
2020-12-09 03:32:23 -0300

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import './App.css';
 import { Button } from 'antd';
+import { Switch, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<Button type="primary">Accept</Button>
-			</header>
-		</div>
+		<Switch>
+			<Route path="/example">
+				<div className="App">
+					<header className="App-header">
+						<p>
+							Edit <code>src/App.tsx</code> and save to reload.
+						</p>
+						<a
+							className="App-link"
+							href="https://reactjs.org"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Learn React
+						</a>
+						<Button type="primary">Accept</Button>
+					</header>
+				</div>
+			</Route>
+			<Route path="/">
+				<div>Home</div>
+			</Route>
+		</Switch>
 	);
 }
 
